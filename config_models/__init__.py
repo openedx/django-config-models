bee31864ae0dfd2760a520cd05b5e1168671dca2
John Eskew <jeskew@edx.org>
2016-10-11 16:21:20 -0400

    Bump to version 0.1.3

--- config_models/__init__.py
+++ config_models/__init__.py
@@ -4,6 +4,6 @@ Configuration models for Django allowing config management with auditing.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 default_app_config = 'config_models.apps.ConfigModelsConfig'  # pylint: disable=invalid-name
