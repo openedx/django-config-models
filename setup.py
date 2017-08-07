4d6fd1063fb6384f2f2ec1b6a04371ae0d97a84a
bmedx <bmesick@edx.org>
2017-08-07 14:25:47 -0400

    Update Django REST Framework dependency to 3.6
    - PLAT-1635

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'django>=1.8,<2',
-        'djangorestframework>=3.2.3,<3.7.0',
+        'djangorestframework>=3.6,<3.7.0',
     ],
     license="AGPL 3.0",
     zip_safe=False,
