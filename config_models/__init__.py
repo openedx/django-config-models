961bafd443d3abafe5b8c82667118407d77c1838
Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2021-01-08 15:33:04 +0500

    Upload token from secrets

--- config_models/__init__.py
+++ config_models/__init__.py
@@ -2,6 +2,6 @@
 Configuration models for Django allowing config management with auditing.
 """
 
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 
 default_app_config = 'config_models.apps.ConfigModelsConfig'  # pylint: disable=invalid-name
