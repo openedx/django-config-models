5fea4f4fb82bdeb0ce68af267e3186fde7cbe4a9
Salman Nawaz <salman.nawaz@A006-01081.local>
2023-07-13 12:44:30 +0500

    fix: django 4.2 upgrade

--- config_models/__init__.py
+++ config_models/__init__.py
@@ -2,6 +2,4 @@
 Configuration models for Django allowing config management with auditing.
 """
 
-__version__ = '2.3.0'
-
-default_app_config = 'config_models.apps.ConfigModelsConfig'  # pylint: disable=invalid-name
+__version__ = '2.4.0'
